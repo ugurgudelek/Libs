bc19eb92c50635ae5216d856d3c90f10e093e18a
Ugur Gudelek <ugurgudelek@gmail.com>
2018-11-25 17:13:28 +0300

    temporary multi dataset bokeh plot

--- src/bokeh_plot.py
+++ src/bokeh_plot.py
@@ -43,12 +43,15 @@ def read_datasets():
 
     return datasets
 
-def make_dataset(dataset, xname, yname):
+def make_dataset(dataset, location_name, line_color):
     """
         element: C 1 @ 279.482
         value : %N,%C,%K,%P
     """
+    xname = display_attribute_selector.value
+    yname = element_selector.value
     df = pd.DataFrame()
+
     df['xs'] = dataset[xname]
     df['ys'] = dataset[yname]
     df['xname'] = xname
@@ -59,14 +62,31 @@ def make_dataset(dataset, xname, yname):
     df['C'] = dataset['%C']
     df['K'] = dataset['%K']
     df['P'] = dataset['%P']
+    df['line_color'] = line_color
+    df['location'] = location_name
     return df
 
-def make_plot(src, xname, yname):
+def make_multi_dataset():
+    d_adana = make_dataset(datasets['adana'], location_name='adana', line_color='blue')
+    d_samsun = make_dataset(datasets['samsun'], location_name='samsun', line_color='green')
+    d_yozgat = make_dataset(datasets['yozgat'], location_name='yozgat', line_color='red')
+    d_nigde = make_dataset(datasets['nigde'], location_name='nigde', line_color='yellow')
+
+    df = d_adana.append(d_samsun, ignore_index=True)
+    df = df.append(d_yozgat, ignore_index=True)
+    df = df.append(d_nigde, ignore_index=True)
+
+    return df
+
+
+
+def make_plot(src):
     p = figure(plot_height=700, plot_width=700, title="Kalibrasyon Grafikleri",
                toolbar_location="below",
                toolbar_sticky=False)
-    p.circle(source=src, x='xs', y='ys', line_color='blue', size=8)
-    hover = HoverTool(tooltips=[("Sample ID", "@sample_id"),
+    p.circle(source=src, x='xs', y='ys', fill_color='line_color', size=8, legend='location')
+    hover = HoverTool(tooltips=[("Loc", "@location"),
+                                ("Sample ID", "@sample_id"),
                                 ("Lab No", "@lab_no"),
                                 ("%N","@N"),
                                 ("%C","@C"),
@@ -80,16 +100,16 @@ def make_plot(src, xname, yname):
     return p
 
 def update(attr, old, new):
-    xname = display_attribute_selector.value
-    yname = element_selector.value
 
-    new_source = make_dataset(datasets[loc_selector.value],
-                              xname,
-                              yname)
-    new_source = ColumnDataSource(new_source)
-    src.data.update(new_source.data)
-    p.xaxis.axis_label = xname
-    p.yaxis.axis_label = yname
+    if MULTI:
+        new_source = make_multi_dataset()
+    else:
+        new_source = make_dataset(datasets[loc_selector.value],
+                                  location_name = loc_selector.value,
+                                  line_color = 'blue')
+    src.data.update(ColumnDataSource(new_source).data)
+    p.xaxis.axis_label =  display_attribute_selector.value
+    p.yaxis.axis_label =  element_selector.value
 
 loc_names = ['adana','samsun','yozgat', 'nigde']
 datasets = read_datasets()
@@ -111,18 +131,28 @@ display_attribute_selector.on_change('value', update)
 element_selector.on_change('value', update)
 controls = widgetbox([loc_selector, display_attribute_selector, element_selector])
 
-# initial dataset
-xname = display_attribute_selector.value
-yname = element_selector.value
 
-src = ColumnDataSource(make_dataset(datasets[loc_selector.value],
-                              xname,
-                              yname))
-p = make_plot(src,xname,yname)
-p.xaxis.axis_label = xname
-p.yaxis.axis_label = yname
 
+# one-by-one plot
+# MULTI=False
+# src = ColumnDataSource(make_dataset(datasets[loc_selector.value], location_name=loc_selector.value, line_color='blue'))
+# p = make_plot(src)
+# p.xaxis.axis_label =  display_attribute_selector.value
+# p.yaxis.axis_label =  element_selector.value
+# layout = row(controls, p)
+
+
+# all-in-one plot
+MULTI=True
+src = ColumnDataSource(make_multi_dataset())
+p = make_plot(src)
+p.xaxis.axis_label =  display_attribute_selector.value
+p.yaxis.axis_label =  element_selector.value
 layout = row(controls, p)
+
+
+p.legend.location = "top_right"
+p.legend.click_policy="hide"
 
 tab = Panel(child=layout, title="Kalibrasyon")
 tabs = Tabs(tabs=[tab])
